b86ed3365a02d3a7c8ae15452cdcd5bab51aae8c
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-04 11:32:29 -0300

    Feat: Create controllers product and sales tests

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -1,1 +1,68 @@
-                           
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const productController = require('../../../controllers/ProductController');
+const productService = require('../../../services/ProductService');
+
+describe('Testa o ProductController', () => {
+describe('Verifica se retorna os produtos corretamente', () => {
+  const res = {};
+  const req = {};
+
+  const arrayProducts = [
+    {
+      "id": 1,
+      "name": "produto",
+      "quantity": 10
+    },
+    {
+      "id": 2,
+      "name": "Outro produto",
+      "quantity": 20
+    },
+  ];
+
+  before(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+  });
+
+  describe('Verifica se retorna todos os produtos', async () => {
+    before(() => {
+      sinon.stub(productService, 'getAll').resolves(arrayProducts);
+    });
+
+    after(() => {
+      productService.getAll.restore();
+    });
+
+    it('Espera que o status retorne o código 200', async () => {
+      await productController.getAll(req, res);
+      expect(res.status.calledWith(200)).to.be.equal(true);
+      expect(res.json.calledWith(arrayProducts)).to.be.equal(true);
+    });
+  });
+  describe('Verifica se retorna um produto através do id', () => {
+    req.params = { id: 1 };
+    const productWithId = [
+      { 
+        "id": 1, 
+        "name": "Produto", 
+        "quantity": 10  
+      }
+    ];
+    before(() => {
+      sinon.stub(productService, 'getById').resolves(productWithId);
+    });
+
+    after(() => {
+      productService.getById.restore();
+    });
+
+    it('Verifica se retorna um objeto com o produto', async () => {
+      await productController.getById(req, res);
+      expect(res.status.calledWith(200)).to.be.equal(true);
+    });
+  });
+});
+});
---
+++ test/unit/controllers/salesController.js
@@ -0,0 +1,117 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const SaleService = require('../../../services/SaleService');
+const SaleController = require('../../../controllers/SaleController');
+
+describe('Testa o SaleController', () => {
+  const response = {};
+  const request = {};
+
+  describe('Verifica se a função getAll mostra quando a sale não é encontrada', () => {
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SaleService, 'getAll').resolves(false);
+    });
+
+    after(async () => {
+      SaleService.getAll.restore();
+    });
+
+    it('Espera que o status retorne o código 404', async () => {
+      await SaleController.getAll(request, response);
+      expect(response.status.calledWith(404)).to.be.equal(true);
+    });
+
+    it('Espera que o json retorne a mensagem "Sale not found"', async () => {
+      await SaleController.getAll(request, response);
+      expect(response.json.calledWith({message: 'Sale not found' })).to.be.equal(true);
+    });
+  });
+
+  describe('Verifica se a função getAll retorna todas as vendas', () => {
+    const getSales = [
+      {
+        "date": "2021-07-05T04:54:24.000Z",
+        "productId": 1,
+        "quantity": 4
+      },
+      {
+        "date": "2021-07-05T04:54:23.000Z",
+        "productId": 2,
+        "quantity": 8
+      },
+      {
+        "date": "2021-07-05T04:54:21.000Z",
+        "productId": 3,
+        "quantity": 3
+      }
+    ]
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SaleService, 'getAll').resolves(getSales);
+    });
+
+    after(async () => {
+      SaleService.getAll.restore();
+    });
+
+    it('Espera que o status retorne o código 200', async () => {
+      await SaleController.getAll(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('Espera que com o json retorne o array com todas as vendas', async () => {
+      await SaleController.getAll(request, response);
+      expect(response.json.calledWith(getSales)).to.be.equal(true);
+    });
+  });
+ 
+  describe('Verifica se a função getById retorna as vendas por Id', () => {
+    request.params = { id: 1 };
+
+    const getSaleById = [
+      {
+        "date": "2021-07-05T04:54:24.000Z",
+        "productId": 1,
+        "quantity": 4
+      },
+      {
+        "date": "2021-07-05T04:54:23.000Z",
+        "productId": 2,
+        "quantity": 8
+      },
+      {
+        "date": "2021-07-05T04:54:21.000Z",
+        "productId": 3,
+        "quantity": 3
+      }
+    ]
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SaleService, 'getById').resolves(getSaleById);
+    });
+
+    after(async () => {
+      SaleService.getById.restore();
+    });
+
+    it('Espera que o status retorne o código 200', async () => {
+      await SaleController.getById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('Espera que com o json retorne o array de vendas', async () => {
+      await SaleController.getById(request, response);
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+  });
+});
